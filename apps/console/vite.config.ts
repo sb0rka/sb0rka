7170e325675d4a7bf59839e1fffa030149e96513
Anton Mushnin <anton.mushnin@gmail.com>
2026-04-14 22:12:14 +0300

    Add Tailwind CSS configuration and update dependencies
    - Introduced Tailwind CSS configuration in tailwind.config.js for styling.
    - Updated package.json and package-lock.json to include new dependencies for Radix UI components and Tailwind CSS.
    - Enhanced TypeScript configuration with baseUrl and paths for easier imports.
    - Updated Vite configuration to integrate Tailwind CSS and autoprefixer.

--- apps/console/vite.config.ts
+++ apps/console/vite.config.ts
@@ -1,9 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path"
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import tailwindcss from "tailwindcss"
+import autoprefixer from "autoprefixer"
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss(), autoprefixer()],
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     port: 3000,
     strictPort: true,
