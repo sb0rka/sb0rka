7130e2f4a88a78767a892103f2f863190119d653
kompotkot <zenitsws@gmail.com>
2026-04-26 21:09:19 +0000

    Resource states

--- packages/contract/resource.go
+++ packages/contract/resource.go
@@ -3,10 +3,17 @@ package contract
 import "time"
 
 type ResourceResponse struct {
-	ID           string    `json:"id"`
-	ProjectID    string    `json:"project_id"`
-	IsActive     bool      `json:"is_active"`
-	ResourceType string    `json:"resource_type"`
+	ID        string    `json:"id"`
+	ProjectID string    `json:"project_id"`
+	IsActive  bool      `json:"is_active"`
+	Kind      string    `json:"kind"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type ResourceStateResponse struct {
+	ResourceID   string    `json:"resource_id"`
+	RuntimeState string    `json:"runtime_state"`
 	CreatedAt    time.Time `json:"created_at"`
 	UpdatedAt    time.Time `json:"updated_at"`
 }
